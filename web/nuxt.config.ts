6cbeaab1bc9a44ff8920dd05cc12a7bf4268686a
phanlyhuynh <lyhuynh0806@gmail.com>
2023-09-14 21:31:53 +0700

    init

---
+++ web/nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
