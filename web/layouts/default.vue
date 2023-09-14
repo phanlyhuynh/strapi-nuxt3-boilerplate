5f9840ec10b743f7217d551f11438beda518d4a4
phanlyhuynh <lyhuynh0806@gmail.com>
2023-09-15 00:29:02 +0700

    Update ui

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -1,68 +1,49 @@
 <template>
   <div>
+    <!-- header -->
+    <header class="py-4 shadow-sm bg-white">
+      <div class="container flex items-center justify-between">
+        <nuxt-link to="/">Logo</nuxt-link>
+
+        <div class="w-full max-w-xl relative flex">
+          <span class="absolute left-4 top-3 text-lg text-gray-400">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </span>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            class="w-full border border-primary border-r-0 pl-12 py-3 pr-3 rounded-l-md focus:outline-none hidden md:flex"
+            placeholder="search"
+          />
+          <button
+            class="flex items-center bg-primary border border-primary text-white px-8 rounded-r-md hover:bg-transparent hover:text-primary transition hidden md:flex"
+          >Search</button>
+        </div>
+
+        <div class="flex items-center space-x-4">
+          <nuxt-link to="/checkout" class="text-center text-gray-700 hover:text-primary transition relative pointer">
+            <div class="text-2xl">
+              <i class="fa-regular fa-heart"></i>
+            </div>
+            <div class="text-xs leading-3">Checkout</div>
+          </nuxt-link>
+          <nuxt-link to="/cart" class="text-center text-gray-700 hover:text-primary transition relative">
+            <div class="text-2xl">
+              <i class="fa-solid fa-bag-shopping"></i>
+            </div>
+            <div class="text-xs leading-3">Cart</div>
+          </nuxt-link>
+          <a href="#" class="text-center text-gray-700 hover:text-primary transition relative">
+            <div class="text-2xl">
+              <i class="fa-regular fa-user"></i>
+            </div>
+            <div class="text-xs leading-3">Account</div>
+          </a>
+        </div>
+      </div>
+    </header>
+    <!-- ./header -->
     <slot />
   </div>
 </template>
-
-<style>
-html {
-  /* font-family: 'Nunito', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-        Roboto, 'Helvetica Neue', Arial, sans-serif; */
-  font-family: "Nunito";
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-.button--hero {
-  display: inline-block;
-  color: #fff;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-.button--green {
-  display: inline-block;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-.button--grey {
-  display: inline-block;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-.button--delete {
-  display: inline-block;
-  border: 1px solid #35495e;
-  padding: 5px;
-  color: white;
-  background-color: #35495e;
-}
-button:focus {
-  outline: none;
-}
-.container {
-  width: 80%;
-}
-</style>
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -9,7 +9,25 @@ module.exports = {
     "./app.vue",
   ],
   theme: {
-    extend: {},
+    screen: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        primary: "#fd3d57",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms'),],
 }
